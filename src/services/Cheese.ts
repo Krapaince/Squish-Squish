d59c6880fc1f162f37fca68def35c2d88c70302a
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-12-13 21:45:18 +0100

    Add prettierrc.

--- src/services/Cheese.ts
+++ src/services/Cheese.ts
@@ -1,6 +1,5 @@
 import { Cheese } from '../types/cheese'
 
-
 export async function getCheeses(): Promise<Array<Cheese>> {
   const query = `
   SELECT DISTINCT ?cheese ?label ?country ?source ?thumbnail
@@ -13,30 +12,31 @@ export async function getCheeses(): Promise<Array<Cheese>> {
             dbo:thumbnail ?thumbnail .
     FILTER (lang(?label) = "en")
     }
-  `;
+  `
   const results = await fetch(`https://dbpedia.org/sparql?query=${query}`, {
     method: 'GET',
     headers: {
-      "Accept": "application/sparql-results+json"
-    }
+      Accept: 'application/sparql-results+json',
+    },
   })
-  .then(async function (data) {
-    return data.text().then(JSON.parse);
-
-  })
-  .catch(function (error) {
-    console.log('Request failed', error);
-  });
+    .then(async function (data) {
+      return data.text().then(JSON.parse)
+    })
+    .catch(function (error) {
+      console.log('Request failed', error)
+    })
   console.log(results.results)
-  return results.results.bindings.map((binding:unknown) => {
-    return {
-      link: binding.cheese.value,
-      label: binding.label.value,
-      country: binding.country.value,
-      source: binding.source.value,
-      thumbnail: binding.thumbnail.value
-    };
-  }).sort(function compareFn(elem1: Cheese, elem2: Cheese) {
-    return elem1.label.localeCompare(elem2.label)
-  })
+  return results.results.bindings
+    .map((binding: unknown) => {
+      return {
+        link: binding.cheese.value,
+        label: binding.label.value,
+        country: binding.country.value,
+        source: binding.source.value,
+        thumbnail: binding.thumbnail.value,
+      }
+    })
+    .sort(function compareFn(elem1: Cheese, elem2: Cheese) {
+      return elem1.label.localeCompare(elem2.label)
+    })
 }
